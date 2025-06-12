2402fbe160c19251f77776a963b434ea37c84e6a
kdh4970 <kdh4970@gmail.com>
2025-06-12 10:56:35 +0900

    update

--- run_sim.sh
+++ run_sim.sh
@@ -6,7 +6,7 @@ wd=$PWD
 # Find ROS and Launch Rviz2
 rviz_config="./rviz_config/config.rviz"
 ros_path1="/opt/ros/humble/local_setup.zsh"
-ros_path2="/home/do/ros2_humble/install/local_setup.zsh"
+ros_path2="$HOME/ros2_humble/install/local_setup.zsh"
 if  [ -f "$ros_path1" ]; then
 	nohup zsh -c "source $ros_path1; rviz2 -d $rviz_config" > rviz.log 2>&1 &
 elif [ -f "$ros_path2" ]; then
@@ -35,7 +35,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$isaacsim_path/exts/isaacsim.ros2.bridge
 
 ## IsaacSIM
 cd $isaacsim_path
-#exec /home/do/isaacsim/python.sh /home/do/isaacsim/test_scripts/run.py
-#exec /home/do/isaacsim/python.sh /home/do/isaacsim/standalone_examples/api/isaacsim.ros2.bridge/carter_stereo.py
 $isaacsim_path/python.sh $wd/scripts/dependecy_check.py
 $isaacsim_path/python.sh $wd/scripts/robot_simulation.py
